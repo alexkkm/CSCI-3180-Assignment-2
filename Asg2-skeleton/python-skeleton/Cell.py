0a142e276a7b57b78ce162aaa2c2a2c3f769ce8a
alexkkm <alexkong0222@gmail.com>
2022-02-24 15:35:48 +0800

    init

--- Asg2-skeleton/python-skeleton/Cell.py
+++ Asg2-skeleton/python-skeleton/Cell.py
@@ -1,4 +1,7 @@
 from abc import abstractmethod
+from sqlalchemy import null, true
+
+from GameCharacter import GameCharacter
 
 
 class Cell:
@@ -6,37 +9,45 @@ class Cell:
         self._row = row
         self._col = col
         self._occupant = None
-        self._color = None 
+        self._color = None
         self._hours = 0
-    
+
     # TODO: hours getter
 
     def set_occupant(self, obj):
-        # TODO: set occupant for the Plain cell 
+        # TODO: set occupant for the Plain cell
         #       return whether success or not
+        if self.occupant() == null:
+            self._occupant = obj
+            return true
+        else:
+            if isinstance(self.occupant(), GameCharacter):
+                if GameCharacter.interact_with(obj)
 
         # END TODO
 
     def remove_occupant(self):
-        # TODO: remove the occupant 
-
+        # TODO: remove the occupant
+        self._occupant = null
         # END TODO
 
-    @property 
+    @property
     def occupant(self):
         return self._occupant
 
     def display(self):
-        # TODO: print a string to display the cell 
-        #       and the occupant in the cell 
+        # TODO: print a string to display the cell
+        #       and the occupant in the cell
 
         # END TODO
+
 
 class Plain(Cell):
     def __init__(self, row, col):
         Cell.__init__(self, row, col)
         self._color = '\033[1;32;42m'
         self._hours = 1
+
 
 class Mountain(Cell):
     def __init__(self, row, col):
@@ -47,7 +58,8 @@ class Mountain(Cell):
         # TODO: return False
 
         # END TODO
-    
+
+
 class Swamp(Cell):
     def __init__(self, row, col):
         Cell.__init__(self, row, col)
