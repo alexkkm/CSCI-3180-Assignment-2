e7a81eb32451d5a05ad7327a0394d67f499d7d60
alexkkm <alexkong0222@gmail.com>
2022-03-01 02:11:58 +0800

    finished Map.py

--- Asg2-skeleton/python-skeleton/Map.py
+++ Asg2-skeleton/python-skeleton/Map.py
@@ -1,4 +1,7 @@
 from xml.etree import ElementInclude
+from sqlalchemy import true
+
+from tables import Cols
 from Cell import Cell
 
 
@@ -7,37 +10,53 @@ class Map:
         self._rows = rows
         self._cols = cols
         self._cells = [[Cell() for x in range(cols)] for y in range(rows)]
-    
-    #TODO: rows getter
-    
-    #TODO: cols getter
-    
-    def get_cell(self, row, col):
-        #TODO: 
-        if # condition
-            print("\033[1;31;46mNext move is out of boundary!\033[0;0m")
-            return None 
-        else:
-            # return a cell 
-        # END TODO 
 
-    def build_cell(self, row, col, cell):
-        #TODO:
-        if # condition
-            print("\033[1;31;46mThe position (%d, %d) is out of boundary!\033[0;0m" %(row, col))
-            return False 
+    # rows getter
+    def get_rows(self){
+        return self._rows
+    }
+
+    # cols getter
+    def get_cols(self){
+        return self._cols
+    }
+
+    def get_cell(self, row, col):
+        if (row < 0 or row >= self._rows or col < 0 or col >= self._cols):  # condition
+            print("\033[1;31;46mNext move is out of boundary!\033[0;0m")
+            return None
         else:
-            # return a cell 
-        # END TODO
+            # return a cell
+            return self._cells[row][col]
+            # END
+
+    # TODO: !!!need to determine whether return a bool or return a cell
+    def build_cell(self, row, col, cell):
+        #
+        if (row < 0 or row >= self._rows or col < 0 or col >= self._cols):  # condition
+            print(
+                "\033[1;31;46mThe position (%d, %d) is out of boundary!\033[0;0m" % (row, col))
+            return False
+        else:
+            self._cells[row][col] = cell
+            return True
+            # END
 
     def get_neighbours(self, row, col):
         return_cells = []
-        # TODO: 
 
-        # END TODO
-        
+        for i in range(max(0, row-1), min(row+1, self._rows-1)+1):
+            for j in range(max(0, col-1), min(col+1, self.cols-1)+1):
+                return_cells.append(self._cells[i][j])
+
+        return return_cells
 
     def display(self):
-        # TODO: 
-
-        # END TODO
+        for i in range(0, self.cols):
+            print("%d     ", i)
+        print("%n")
+        for i in range(0, self.rows):
+            print("%d ", i)
+            for j in range(0, self.cols):
+                self._cells[i][j].display()
+            print("%n%n")
---
+++ Asg2-skeleton/test.py
@@ -0,0 +1,7 @@
+for i in range(1, 4+1):  # for(i=1;i<=4;i++)
+    print(i)
+
+for i in range(1, 4):  # for(i=1;i<4;i++)
+    print(i)
+
+print("%n")
